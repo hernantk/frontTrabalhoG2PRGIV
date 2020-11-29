1f0e456765df2b90e4ac42140281457f70eb0347
hernan <Hernantkup@gmail.com>
2020-11-29 14:10:31 -0300

    Sala

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlunoComponent } from './ui/aluno/aluno.component';
+import { SalaComponent } from './ui/sala/sala.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:"new-aluno",component: AlunoComponent},
+{path:"new-sala",component: SalaComponent},
+{path:"new-trabalho"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SalaComponent } from './ui/sala/sala.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalaComponent
   ],
   imports: [
     BrowserModule,
